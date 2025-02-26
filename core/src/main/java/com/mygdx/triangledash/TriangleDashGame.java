ac086941245704e80de00b7ad9d7dc5d207a7531
Garett Takeyasu <takeyasu.garett@gmail.com>
2025-02-26 09:28:42 +0900

    setup

---
+++ core/src/main/java/com/mygdx/triangledash/TriangleDashGame.java
@@ -0,0 +1,7 @@
+package com.mygdx.triangledash;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class TriangleDashGame extends ApplicationAdapter {
+}
